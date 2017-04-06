3491f0d3a1bbf7935a9aafdd55930d9e24f496f7
Pedro Reis <pedroreisv@gmail.com>
2017-04-06 08:34:21 -0300

    Connect to firebase and setup to actions and reducers

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 class Main extends Component {
+    componentWillMount() {
+        var config = {
+            apiKey: "AIzaSyBI0wzws5u-kwWEd-HLsick5LOUUafPjWU",
+            authDomain: "shrtr-a4744.firebaseapp.com",
+            databaseURL: "https://shrtr-a4744.firebaseio.com",
+            projectId: "shrtr-a4744",
+            storageBucket: "shrtr-a4744.appspot.com",
+            messagingSenderId: "818860627919"
+        }
+        firebase.initializeApp(config)
+    }
+
     render() {
         return (
             <div>
