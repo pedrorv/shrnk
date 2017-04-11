016a5beef248fd4184505586e4ecf5f98a71e9ca
Pedro Reis <pedroreisv@gmail.com>
2017-04-11 14:42:44 -0300

    Add test to updateLinkAccessCount

--- test/api/api.test.js
+++ test/api/api.test.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase'
 import { expect } from 'chai'
 
-import { loggedUser } from '../../app/api'
+import { loggedUser, getLinkInfo, updateLinkAccessCount } from '../../app/api'
+
+const testingObjectID = '123456'
 
 describe('API', () => {
   before(() => {
@@ -23,6 +25,43 @@ describe('API', () => {
           expect(user).to.be.a('object')
           expect(user).to.have.property('uid')
           expect(user.uid).to.be.a('string')
+          done()
+        })
+        .catch(done)
+    })
+  })
+
+  describe('getLinkInfo', () => {
+    it('should return a link object', (done) => {
+      // using a default link from testing database. link id: '123456'
+      getLinkInfo(testingObjectID)
+        .then(link => {
+          expect(link).to.be.a('object')
+          expect(link).to.have.property('key')
+          expect(link).to.have.property('linkInfo')
+          expect(link.linkInfo.id).to.equal(testingObjectID)
+          done()
+        })
+        .catch(done)
+    })
+  })
+
+  describe('updateLinkAccessCount', () => {
+    let testingLink
+    before((done) => {
+      getLinkInfo(testingObjectID)
+        .then(link => {
+          testingLink = link
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should increase testingObject access_count by one', (done) => {
+      updateLinkAccessCount(testingLink.key)
+        .then(() => getLinkInfo(testingLink.linkInfo.id))
+        .then(link => {
+          expect(link.linkInfo.access_count).to.equal(testingLink.linkInfo.access_count + 1)
           done()
         })
         .catch(done)
