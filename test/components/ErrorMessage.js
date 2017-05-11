0c1640c7fcc488928c324e37bc0de1ceb7c244bd
Pedro Reis <pedroreisv@gmail.com>
2017-05-11 19:00:55 -0300

    Add ErrorMessage component tests

---
+++ test/components/ErrorMessage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+
+import ErrorMessage from '../../app/components/ErrorMessage'
+
+describe('<ErrorMessage />', () => {
+
+  it('should render component', () => {
+    const wrapper = shallow(<ErrorMessage />)
+    expect(wrapper.find('article.message.is-danger')).to.have.length(1)
+    expect(wrapper.find('div.message-body')).to.have.length(1)
+  })
+
+  it('should render component with the correct error message', () => {
+    const errorMessage = "Some error message"
+    const wrapper = shallow(<ErrorMessage error={errorMessage} />)
+    expect(wrapper.find('div.message-body').text()).to.be.equal(errorMessage)
+  })
+
+})
